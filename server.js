a929b8cdebe2d282bc159e4c2f0ec67f999cac92
Ubuntu <ubuntu@dockerlinuxsolo.44d0guzcwyaunpjnbjte52ceqf.bx.internal.cloudapp.net>
2017-05-20 17:57:54 +0000

    new error

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not found go somewhere else.');
+  var err = new Error('Not found go away.');
   err.status = 404;
   next(err);
 });
